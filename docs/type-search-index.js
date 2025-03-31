7986cf882304426e6e238d86e8df3bcffdec5de0
Andres Barrero <fbarrero1358@gmail.com>
2025-03-31 22:01:31 +0200

    creación de JavaDoc del test

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"resta","l":"Resta"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Main"},{"p":"resta","l":"Resta"},{"p":"resta","l":"RestaTest"}];updateSearchResults();
